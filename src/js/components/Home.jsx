6f35b81b90a7ba7fdc43a26d1eb3eb754f33ad61
Santiago Ispizua <129971384+sispizua@users.noreply.github.com>
2026-01-07 21:43:52 +0000

    funciones de newTask,getAllTask, deleteTask,createTask,createUser,handleSubmit,handleInputChange

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,130 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+    const API_URL = "https://playground.4geeks.com/todo";
+    const USER = "santiago";
+    const [newTask, setTask] = useState({ label: "", is_done: false });
+    const [tasks, setTasks] = useState([]);
+    console.log(tasks);
+    const [showAlert, setShowAlert] = useState(false)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const handleInputChange = (e) => {
+        console.log(e)
+        setTask({
+            ...newTask,
+            [e.target.name]: e.target.value,
+        });
+    };
+    // funcion para evitar el submit por defecto y mostrar una alerta si el campo esta vacio
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!newTask.label) {
+            setShowAlert(true);
+            return
+        }
+        setShowAlert(false)
+        createTask();
+    }
+    //obtener todas las tareas
+    const getAllTasks = async () => {
+        const response = await fetch(`${API_URL}/users/${USER}`)
+        console.log(response);
+        //crear usuario si no existe
+        if (!response.ok) {
+            console.log("debo crear al usuario");
+            createUser();
+            return
+        }
+        const data = await response.json()
+        console.log(data);
+        setTasks(data.todos || []);
+    }
+
+    // funcion para crear usuario
+    const createUser = async () => {
+        const response = await fetch(`${API_URL}/users/${USER}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+
+        )
+        console.log(response);
+        if (!response.ok) {
+            console.log("debo crear al usuario");
+            return
+        }
+        const data = await response.json()
+        console.log(data);
+    }
+    const createTask = async () => {
+        const response = await fetch(`${API_URL}/todos/${USER}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newTask),
+            headers: {
+                "content-type": "application/json"
+            }
+        });
+        if (response.ok) {
+            setTask({ label: "", is_done: false });
+            getAllTasks();
+        } else {
+            console.log("Error creando tarea:", response.status);
+        }
+    };
+    const deleteTask = async (id) => {
+        const response = await fetch(`${API_URL}/todos/${id}`, {
+            method: "DELETE"
+        });
+        if (response.ok) {
+            setTasks((prev) => prev.filter((t) => t.id !== id));
+        }
+    }
+    const cleanTasks = async () => {
+        const response = await fetch(`${API_URL}/todos/${id}`, {
+            method: "DELETE"
+        });
+        if (response.ok) {
+           getAllTasks();
+        }
+    }
+    //montaje de todas las tareas 
+    useEffect(() => {
+        getAllTasks()
+    }, [])
+    return (
+        <div className="row">
+            {showAlert && (
+                <div className="alert alert-warning" role="alert">
+                    No hay ninguna tarea, por favor inerte un tarea.
+                </div>
+            )}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="tarea">Tarea</label>
+                <input
+                    id="tarea"
+                    type="text"
+                    name="label"
+                    value={newTask.label}
+                    onChange={handleInputChange}
+                />
+            </form>
+            {tasks.map((item) => (
+                <div key={item.id} className="task">
+                    {item.label}
+                    <button type="button" onClick={() => deleteTask(item.id)}><i className="fa-solid fa-trash"></i></button>
+                </div>
+
+            ))}
+
+            <div>
+                <button type="button" onClick={()=> cleanTasks()} className="btn btn-primary">Limpiar lista de Tareas</button>
+            </div>
+        </div>
+    );
+
 };
 
 export default Home;
